623171334658e8cfb80a1afcf0b0a42804ca8339
CaseyKelly <casey.kelly@colorado.edu>
2015-01-21 12:36:50 -0700

    added terms and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'about/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
 
   root 'welcome#index'
+  get 'about' => 'about#show'
+  get 'terms' => 'terms#show'
   get 'faq' => 'faq#show'
 
   # Example of regular route:
