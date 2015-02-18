beba0d5a5bd24ae7927ec56d045387f114a361f0
CaseyKelly <casey.kelly@colorado.edu>
2015-02-18 14:44:32 -0700

    Spec to create projects passes

---
+++ spec/features/crud_projects_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'Users can CRUD projects' do
+
+  before :each do
+    @project = Project.create(name: 'Test project')
+    visit '/projects'
+    expect(page).to have_content 'Test project'
+  end
+
+  it "user can create a project" do
+    click_on 'Create Project'
+    expect(page).to have_content 'Name'
+    click_button "Create Project"
+    expect(page).to have_content 'error prohibited this from being saved'
+    fill_in "Name", with: 'Big project'
+    click_button "Create Project"
+    expect(page).to have_content 'Project was successfully created.'
+  end
+
+  it 'user can edit a project' do
+
+  end
+
+  it 'user can delete a project' do
+
+  end
+
+end
