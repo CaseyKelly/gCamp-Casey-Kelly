b8fc141fb7ab2b982a732bf9eecf891cd61acc1b
CaseyKelly <casey.kelly@colorado.edu>
2015-02-18 14:15:19 -0700

    Spec to create user passes

---
+++ spec/features/crud_users_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'Users can CRUD users' do
+
+  before :each do
+    @user = User.create(first_name: 'Johnny', last_name: 'Cash', email: 'johnny@cash.com')
+    visit '/users'
+    expect(page).to have_content 'Johnny Cash'
+  end
+
+  it "user can create a user" do
+    click_on 'New User'
+    expect(page).to have_content 'First name'
+    click_button "Create User"
+    expect(page).to have_content 'errors prohibited this task from being saved'
+    fill_in "First name", with: 'Casey'
+    fill_in "Last name", with: 'Kelly'
+    fill_in "Email", with: 'casey@kelly.com'
+    click_button "Create User"
+    expect(page).to have_content 'User was successfully created.'
+  end
+
+  it 'user can edit a user' do
+
+  end
+
+  it 'user can delete a user' do
+
+  end
+
+end
