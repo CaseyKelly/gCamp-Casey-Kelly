3f10d5f6e5a9b69736b0d26652f966fbc92bce0d
CaseyKelly <casey.kelly@colorado.edu>
2015-02-18 16:05:20 -0700

    Users can create new users through signup page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:alert] = "Denied bitch!"
+      render :new
+    end
+  end
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   resources :tasks, :users, :projects
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
