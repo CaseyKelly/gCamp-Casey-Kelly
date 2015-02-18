2b8edb1582bc7fc9c250c87b5dc64f4d9d8c23d6
CaseyKelly <casey.kelly@colorado.edu>
2015-02-18 13:57:40 -0700

    Spec for user to edit task passes

--- spec/features/crud_tasks_spec.rb
+++ spec/features/crud_tasks_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe 'Users can CRUD tasks' do
 
   before :each do
+    @task = Task.create(description: 'Task hard bro', due_date: '08-19-2015', complete: false)
     visit "/tasks"
-    expect(page).to have_content 'Due On'
+    expect(page).to have_content 'Task hard bro'
   end
 
   it "user can create task" do
@@ -13,6 +14,13 @@ describe 'Users can CRUD tasks' do
     fill_in "Description", with: 'Pass the test'
     click_button "Create Task"
     expect(page).to have_content 'Task was successfully created.'
+  end
+
+  it 'user can edit a task' do
+    visit "/tasks"
+    click_on 'Edit'
+    click_on 'Update Task'
+    expect(page).to have_content 'Task was successfully updated.'
   end
 
 
