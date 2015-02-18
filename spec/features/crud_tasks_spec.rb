c53d6e0ae889eb2111c5a66a156c25a11b2ac078
CaseyKelly <casey.kelly@colorado.edu>
2015-02-18 14:07:53 -0700

    Removed comments in task spec file

--- spec/features/crud_tasks_spec.rb
+++ spec/features/crud_tasks_spec.rb
@@ -30,22 +30,3 @@ describe 'Users can CRUD tasks' do
   end
 
 end
-
-# describe 'add a player' do
-#
-#   before :each do
-#     @my_team = Team.create(name: 'pizza and the woofers')
-#     visit "teams/#{@my_team.id}/players/new"
-#   end
-#
-#   it "successfully adds a player" do
-#     expect(page).to have_content @my_team.name
-#   end
-#
-#   it "adds a player with a name" do
-#     fill_in "Name", with: "Puggeroni"
-#     click_button "Create Player"
-#     expect(page).to have_content "Puggeroni"
-#   end
-#
-# end
