3aaa17c3068e6bd2ee186597591fbce197b2f098
CaseyKelly <casey.kelly@colorado.edu>
2015-02-26 15:15:32 -0700

    Comment out tasks CRUD test, will write when that ability is reintroduced

--- spec/features/crud_tasks_spec.rb
+++ spec/features/crud_tasks_spec.rb
@@ -2,38 +2,39 @@ require 'rails_helper'
 
 describe 'Users can CRUD tasks' do
 
-  before :each do
-    visit '/signup'
-    fill_in "First name", with: 'Charlie'
-    fill_in "Last name", with: 'Bucket'
-    fill_in "Email", with: 'charlie@user.com'
-    fill_in "Password", with: 'password'
-    fill_in "Password confirmation", with: 'password'
-    click_on 'Login!'
-    @task = Task.create(description: 'Task hard bro', due_date: '08-19-2015', complete: false)
-    visit "/tasks"
-    expect(page).to have_content 'Task hard bro'
-  end
-
-  it "user can create task" do
-    click_on 'New Task'
-    expect(page).to have_content 'Description'
-    fill_in "Description", with: 'Pass the test'
-    click_button "Create Task"
-    expect(page).to have_content 'Task was successfully created.'
-  end
-
-  it 'user can edit a task' do
-    visit "/tasks"
-    click_on 'Edit'
-    click_on 'Update Task'
-    expect(page).to have_content 'Task was successfully updated.'
-  end
-
-  it 'user can delete a task' do
-    visit "/tasks"
-    click_on 'Delete'
-    expect(page).to have_content 'Task was successfully destroyed.'
-  end
+  # before :each do
+  #   visit '/signup'
+  #   fill_in "First name", with: 'Charlie'
+  #   fill_in "Last name", with: 'Bucket'
+  #   fill_in "Email", with: 'charlie@user.com'
+  #   fill_in "Password", with: 'password'
+  #   fill_in "Password confirmation", with: 'password'
+  #   click_on 'Login!'
+  #   @project = Project.create(name: 'Test project')
+  #   @task = Task.create(description: 'Task hard bro', due_date: '08-19-2015', complete: false)
+  #   visit project_tasks_path(@project)
+  #   expect(page).to have_content 'Task hard bro'
+  # end
+  #
+  # it "user can create task" do
+  #   click_on 'New Task'
+  #   expect(page).to have_content 'Description'
+  #   fill_in "Description", with: 'Pass the test'
+  #   click_button "Create Task"
+  #   expect(page).to have_content 'Task was successfully created.'
+  # end
+  #
+  # it 'user can edit a task' do
+  #   visit "/tasks"
+  #   click_on 'Edit'
+  #   click_on 'Update Task'
+  #   expect(page).to have_content 'Task was successfully updated.'
+  # end
+  #
+  # it 'user can delete a task' do
+  #   visit "/tasks"
+  #   click_on 'Delete'
+  #   expect(page).to have_content 'Task was successfully destroyed.'
+  # end
 
 end
