038c529052988161bd7ff072d8d353bc475c5742
CaseyKelly <casey.kelly@colorado.edu>
2015-02-26 17:29:23 -0700

    Task default value for completed is false

---
+++ db/migrate/20150227002348_add_default_value_to_complete_task.rb
@@ -0,0 +1,5 @@
+class AddDefaultValueToCompleteTask < ActiveRecord::Migration
+  def change
+    change_column :tasks, :complete, :boolean, :default => false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226211737) do
+ActiveRecord::Schema.define(version: 20150227002348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 20150226211737) do
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
     t.date     "due_date"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.boolean  "complete"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "complete",    default: false
     t.integer  "project_id"
   end
 
