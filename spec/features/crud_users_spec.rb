93c098d793205a1fc2c674046d364db751b1f302
CaseyKelly <casey.kelly@colorado.edu>
2015-03-09 21:22:48 -0600

    Add validation specs for users

--- spec/features/crud_users_spec.rb
+++ spec/features/crud_users_spec.rb
@@ -15,9 +15,6 @@ describe 'Users can CRUD users' do
 
   it "user can create a user" do
     click_on 'New User'
-    expect(page).to have_content 'First name'
-    click_button "Login!"
-    expect(page).to have_content "Password can't be blank"
     fill_in "First name", with: 'Casey'
     fill_in "Last name", with: 'Kelly'
     fill_in "Email", with: 'casey@kelly.com'
@@ -27,17 +24,79 @@ describe 'Users can CRUD users' do
     expect(page).to have_content 'Thanks for signing up, dude!'
   end
 
-  # it 'user can edit a user' do
-  #   visit "/users"
-  #   click_on 'Edit'
-  #   click_on 'Update User'
-  #   expect(page).to have_content 'User was successfully updated.'
-  # end
-
   it 'user can delete a user' do
     visit "/users"
     click_on 'Delete'
     expect(page).to have_content 'You must be logged in to visit that page.'
+  end
+
+  it 'user can not create a user without a first name' do
+    click_on 'New User'
+    fill_in "Last name", with: 'Kelly'
+    fill_in "Email", with: 'casey@kelly.com'
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+    click_button "Login!"
+    expect(page).to have_content "First name can't be blank"
+  end
+
+  it 'user can not create a user without a last name' do
+    click_on 'New User'
+    fill_in "First name", with: 'Casey'
+    fill_in "Email", with: 'casey@kelly.com'
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+    click_button "Login!"
+    expect(page).to have_content "Last name can't be blank"
+  end
+
+  it 'user can not create a user without a password' do
+    click_on 'New User'
+    fill_in "First name", with: 'Casey'
+    fill_in "Last name", with: 'Kelly'
+    fill_in "Email", with: 'casey@kelly.com'
+    click_button "Login!"
+    expect(page).to have_content "Password can't be blank"
+  end
+
+  it "user can not create a user if passwords do not match" do
+    click_on 'New User'
+    fill_in "First name", with: 'Casey'
+    fill_in "Last name", with: 'Kelly'
+    fill_in "Email", with: 'casey@kelly.com'
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'pasword'
+    click_button "Login!"
+    expect(page).to have_content "Password confirmation doesn't match Password"
+  end
+
+  it 'user can not create a user without an email' do
+    click_on 'New User'
+    fill_in "First name", with: 'Casey'
+    fill_in "Last name", with: 'Kelly'
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+    click_button "Login!"
+    expect(page).to have_content "Email can't be blank"
+  end
+
+  it 'user can not create a user if email is not unique' do
+    click_on 'New User'
+    fill_in "First name", with: 'Casey'
+    fill_in "Last name", with: 'Kelly'
+    fill_in "Email", with: 'casey@kelly.com'
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+    click_button "Login!"
+    expect(page).to have_content 'Thanks for signing up, dude!'
+    click_on 'New User'
+    fill_in "First name", with: 'Casey'
+    fill_in "Last name", with: 'Kelly'
+    fill_in "Email", with: 'casey@kelly.com'
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+    click_button "Login!"
+    expect(page).to have_content 'Email has already been taken'
   end
 
 end
