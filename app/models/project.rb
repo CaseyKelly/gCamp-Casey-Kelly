e33a01952411320f1a47dcc18047663cefc03579
CaseyKelly <casey.kelly@colorado.edu>
2015-03-31 12:38:36 -0600

    users can delete projects from the project show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,20 @@ class Project < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
+
+  def num_memberships
+    if self.memberships.length == 1
+      "#{self.memberships.length} membership"
+    else
+      "#{self.memberships.length} memberships"
+    end
+  end
+
+  def num_tasks
+    if self.tasks.length == 1
+      "#{self.tasks.length} task"
+    else
+      "#{self.tasks.length} tasks"
+    end
+  end
 end
