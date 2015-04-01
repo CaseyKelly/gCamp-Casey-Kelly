3227b9313ac75375c02c1d8e760ea94a77318241
CaseyKelly <casey.kelly@colorado.edu>
2015-03-31 21:29:10 -0600

    add faq table to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309195202) do
+ActiveRecord::Schema.define(version: 20150401032532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "faqs", force: :cascade do |t|
+    t.string "question"
+    t.string "answer"
+    t.string "slug"
+  end
 
   create_table "memberships", force: :cascade do |t|
     t.integer "user_id"
