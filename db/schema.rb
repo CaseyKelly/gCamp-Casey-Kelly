04fbf6abaf9e0bef4d280def0582e4423a22fbe0
CaseyKelly <casey.kelly@colorado.edu>
2015-04-06 22:55:49 -0600

    add timestamps to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407023429) do
+ActiveRecord::Schema.define(version: 20150407045403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.text    "message"
-    t.integer "user_id"
-    t.integer "task_id"
+    t.text     "message"
+    t.integer  "user_id"
+    t.integer  "task_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "comments", ["task_id"], name: "index_comments_on_task_id", using: :btree
