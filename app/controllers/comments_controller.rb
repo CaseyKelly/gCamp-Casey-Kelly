ebbafcc4dddcd1ee145d4d91e7950ac92af1089d
CaseyKelly <casey.kelly@colorado.edu>
2015-04-06 21:14:28 -0600

    user name in comment links to user show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,20 @@
 class CommentsController < ApplicationController
+  def create
+    @task = Task.find(params[:task_id])
+    @comment = Comment.new(comment_params)
+    @comment.task_id = params[:task_id]
+    @comment.user_id = current_user.id
+    @project = @comment.task.project
+      if @comment.save
+        redirect_to project_task_path(@project, @task), notice: 'Comment was successfully created.'
+      else
+        render :new
+      end
+  end
 
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message, :user_id, :task_id)
+  end
 end
