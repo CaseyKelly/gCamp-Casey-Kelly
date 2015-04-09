10176ad376eae2f99cbd25fe75afe75c1ed4acf4
CaseyKelly <casey.kelly@colorado.edu>
2015-04-09 16:50:06 -0600

    add spec for gcamp navbar link to projects

--- spec/features/permissions_layouts_spec.rb
+++ spec/features/permissions_layouts_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 describe 'Different users see different layouts' do
 
-  it 'marketing pages have old layout' do
+  before :each do
     visit root_url
+  end
+
+  it 'marketing pages have old layout' do
     expect(page).to have_css '.img-footer'
+  end
+
+  it 'gCamp navbar brand links to projects on internal pages' do
+    @user = User.create(first_name: 'Test', last_name: 'User', email:'test@user.com', password: 'password', password_confirmation: 'password')
+    visit '/login'
+    fill_in "Email", with: 'test@user.com'
+    fill_in "Password", with: 'password'
+    click_on 'Login!'
+    expect(page).to have_content 'Welcome back, stranger!'
+    click_link 'gCamp'
+    expect(page).to have_content 'Create Project'
   end
 
 end
