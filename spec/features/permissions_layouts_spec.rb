4cc53b67f46d911d7e9d8142aff44f16c0a72659
CaseyKelly <casey.kelly@colorado.edu>
2015-04-09 16:52:39 -0600

    refactor permission specs

--- spec/features/permissions_layouts_spec.rb
+++ spec/features/permissions_layouts_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 describe 'Different users see different layouts' do
 
   before :each do
+    @user = User.create(first_name: 'Test', last_name: 'User', email:'test@user.com', password: 'password', password_confirmation: 'password')
+    visit '/login'
+    fill_in "Email", with: 'test@user.com'
+    fill_in "Password", with: 'password'
+    click_on 'Login!'
+    expect(page).to have_content 'Welcome back, stranger!'
     visit root_url
   end
 
@@ -11,14 +17,12 @@ describe 'Different users see different layouts' do
   end
 
   it 'gCamp navbar brand links to projects on internal pages' do
-    @user = User.create(first_name: 'Test', last_name: 'User', email:'test@user.com', password: 'password', password_confirmation: 'password')
-    visit '/login'
-    fill_in "Email", with: 'test@user.com'
-    fill_in "Password", with: 'password'
-    click_on 'Login!'
-    expect(page).to have_content 'Welcome back, stranger!'
     click_link 'gCamp'
     expect(page).to have_content 'Create Project'
+  end
+
+  it 'dropdown only shows projects the current user belongs to' do
+
   end
 
 end
