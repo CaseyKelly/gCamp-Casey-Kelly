3d0e8a75ba295efd43555766a37be8f33c04024a
CaseyKelly <casey.kelly@colorado.edu>
2015-04-12 18:56:39 -0600

    add spec for admin users to see admin checkbox on new user page

--- spec/features/permissions_spec.rb
+++ spec/features/permissions_spec.rb
@@ -174,4 +174,16 @@ describe 'different users have different permissions' do
     expect(page).not_to have_content 'test@user.com'
   end
 
+  it 'there is an admin checkbox on new page for admin users' do
+    @user = User.create(first_name: 'Joe', last_name: 'Admin', email: 'joe@admin.com', password: '123', password_confirmation: '123', admin: true)
+    visit '/login'
+    fill_in "Email", with: 'joe@admin.com'
+    fill_in "Password", with: '123'
+    click_on 'Login!'
+    expect(page).to have_content 'Welcome back, stranger!'
+    visit '/users'
+    click_on 'New User'
+    expect(page).to have_content 'Admin'
+  end
+
 end
